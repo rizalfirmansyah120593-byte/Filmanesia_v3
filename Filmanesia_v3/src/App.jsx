170aade6d2e83cdfe483cfcbea82274efdb6ef27
Rizal Firmansyah <rizalfirmansyah120593@gmail.com>
2026-09-22 13:31:43 +0700

    blog baru

--- Filmanesia_v3/src/App.jsx
+++ Filmanesia_v3/src/App.jsx
@@ -35,8 +35,6 @@ function App() {
           <Route path="/watchlist" element={<WatchlistPage />} />
           <Route path="/trending-film" element={<TrendHubPage />} />
           <Route path="/blog" element={<BlogPage />} />
-          <Route path="/blog/film-indonesia-terbaik" element={<FilmArticlePage />} />
-          <Route path="/blog/film-horor-indonesia-paling-seram" element={<FilmArticlePage />} />
           <Route path="/blog/:slug" element={<FilmArticlePage />} />
           <Route path="/jadwal-bioskop/:city" element={<CityCinemaPage />} />
           <Route path="/:slug" element={<EditorialPage />} />
